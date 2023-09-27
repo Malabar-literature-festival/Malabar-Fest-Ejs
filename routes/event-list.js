f508b191b839ef1b9c4a0f4010176213b693ff88
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-09-27 11:46:49 +0530

    added SEO tags in head

--- routes/event-list.js
+++ routes/event-list.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("event-list");
+  const metaTags = [
+    {
+      description:
+        "Join the Malabar Literature Festival MLF, a cultural gathering celebrating the rich histories, legacies, languages, and arts of Malabar and its historic connections with distant lands like Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, and Africa. Explore literature through open forums, talks, and debates. Delve into the lifeworlds of Mappila and Dalit subalterns. Discover a unique literary experience with Book Plus.",
+    },
+    {
+      keywords:
+        "Malabar Literature Festival, MLF, Malabar, Kozhikode Beach, Calicut Beach, Malabar culture, literature, history, languages, arts, Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, Africa, Mappila, Dalit, Book Plus, Malayalam readers",
+    },
+    {
+      author: "Malabar Literature Festival ",
+    },
+    {
+      robots: "index, follow",
+    },
+  ];
+
+  const title =
+    "Malabar Literature Festival | Celebrating History, Language, and Culture";
+  res.render("event-list",{title, metaTags});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,42 @@ const router = express.Router();
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   try {
-    const title = "Welcome to My Website";
+    const metaTags = [
+      {
+        description:
+          "Join the Malabar Literature Festival MLF, a cultural gathering celebrating the rich histories, legacies, languages, and arts of Malabar and its historic connections with distant lands like Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, and Africa. Explore literature through open forums, talks, and debates. Delve into the lifeworlds of Mappila and Dalit subalterns. Discover a unique literary experience with Book Plus.",
+      },
+      {
+        keywords:
+          "Malabar Literature Festival, MLF, Malabar, Kozhikode Beach, Calicut Beach, Malabar culture, literature, history, languages, arts, Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, Africa, Mappila, Dalit, Book Plus, Malayalam readers",
+      },
+      {
+        author: "Malabar Literature Festival ",
+      },
+      {
+        robots: "index, follow",
+      },
+    ];
+
+    const title =
+      "Malabar Literature Festival | Celebrating History, Language, and Culture";
 
     const aboutData = await AboutUs.find();
 
     const speakerData = await Speakers.find();
 
-    const newsData = await News.find()
+    const newsData = await News.find();
 
-    const testimonialData = await Testimonial.find()
-    console.log(testimonialData)
-    res.render("index", { title, aboutData, speakerData, newsData, testimonialData });
+    const testimonialData = await Testimonial.find();
+    console.log(testimonialData);
+    res.render("index", {
+      title,
+      aboutData,
+      speakerData,
+      newsData,
+      testimonialData,
+      metaTags,
+    });
   } catch (error) {
     console.error("Error:", error);
   }
