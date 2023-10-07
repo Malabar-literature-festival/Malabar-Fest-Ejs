9b14b6522c5085c11751026658de8a5cf1084b00
Mohammed Safwan <safwanpklr@gmail.com>
2023-10-07 14:33:25 +0530

    qr added in delegate

---
+++ routes/ejsRoutes/scanner.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const Registration = require("../../models/Registration");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.render("scan");
+});
+
+router.post("/validateqr", async (req, res) => {
+  try {
+    console.log(req.body);
+    let attended = await Registration.findOne({
+      _id: req.body.value,
+      attended: true,
+    });
+
+    if (!attended) {
+      const updateAttended = await Registration.findByIdAndUpdate(
+        { _id: req.body.value },
+        { attended: true }
+      );
+      updateAttended = await updateAttended.save();
+
+      updateddata = await Registration.findOne({
+        _id: req.body.value,
+      });
+      console.log(updateddata);
+    }
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ message: err });
+  }
+});
+
+module.exports = router;
