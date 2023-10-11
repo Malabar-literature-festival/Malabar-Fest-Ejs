ee115e417ff51766c838a6c13f0aea258a1ce616
kirank860 <kkiru889@gmail.com>
2023-10-11 16:52:33 +0530

    contact news gallery updates

--- routes/ejsRoutes/news.js
+++ routes/ejsRoutes/news.js
@@ -5,12 +5,31 @@ var router = express.Router();
 /* GET home page. */
 router.get("/:id", async function (req, res, next) {
   try {
+    const metaTags = [
+      {
+        description:
+          "Join the Malabar Literature Festival MLF, a cultural gathering celebrating the rich histories, legacies, languages, and arts of Malabar and its historic connections with distant lands like Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, and Africa. Explore literature through open forums, talks, and debates. Delve into the lifeworlds of Mappila and Dalit subalterns. Discover a unique literary experience with Book Plus.",
+      },
+      {
+        keywords:
+          "Malabar Literature Festival, MLF, Malabar, Kozhikode Beach, Calicut Beach, Malabar culture, literature, history, languages, arts, Kayalpattinam, Gujarat, Lakshadweep, Andaman and Nicobar, Hadhramaut, Malaysia, Africa, Mappila, Dalit, Book Plus, Malayalam readers",
+      },
+      {
+        author: "Malabar Literature Festival ",
+      },
+      {
+        robots: "index, follow",
+      },
+    ];
+
+    const title =
+      "Malabar Literature Festival | Celebrating History, Language, and Culture";
     const id = req.params.id;
     console.log(id);
     const newsData = await News.findById(id);
     const news = await News.find();
     console.log(newsData);
-    res.render("news", { title: "Express", newsData, news });
+    res.render("news", { title, newsData, metaTags, news });
   } catch (error) {
     console.error(error);
   }
