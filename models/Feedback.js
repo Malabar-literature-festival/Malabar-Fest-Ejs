9d40f8717ce58bdbfb143c61eaf77b61df3534b9
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-11-21 11:29:17 +0530

    updated feedback

---
+++ models/Feedback.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const FeedbackSchema = new mongoose.Schema(
+  {
+    session: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Session",
+    },
+    description: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Registration",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Feedback", FeedbackSchema);
