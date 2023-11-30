097848b2a51022b68f0202c45ded02152b2714fc
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-11-30 09:57:20 +0530

    added notification and  updated scan

---
+++ routes/notification.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+// Controllers
+const {
+    createNotification,
+    getNotification,
+    updateNotification,
+    deleteNotification,
+    // getByFranchise,
+} = require("../controllers/Notification");
+// Middleware
+const { protect, authorize } = require("../middleware/auth");
+const { reqFilter } = require("../middleware/filter");
+const { getS3Middleware } = require("../middleware/s3client");
+const getUploadMiddleware = require("../middleware/upload");
+
+router
+    .route("/")
+    .post(
+        getUploadMiddleware("uploads/notification", ["image"]),
+        getS3Middleware(["image"]),
+        createNotification
+    )
+    .get(reqFilter, getNotification)
+    .put(
+        getUploadMiddleware("uploads/notification", ["image"]),
+        getS3Middleware(["image"]), updateNotification
+    )
+    .delete(deleteNotification);
+
+module.exports = router;
